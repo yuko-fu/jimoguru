f28aa01ba3dd3e7d02b14f4d0fb7e401b9fafdbf
yuko <ryoyu-fu@live.jp>
2023-08-04 14:37:29 +0900

    vwrote js and map in new.html of vote

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -13,7 +13,14 @@ class VotesController < ApplicationController
 
   # GET /votes/new
   def new
-    @vote = Vote.new
+    @shop = Shop.find(params[:shop_id])
+    @existing_vote = Vote.find_by(user: current_user, shop: @shop)
+
+    if @existing_vote
+      redirect_to edit_vote_path(@existing_vote)
+    else
+      @vote = Vote.new(shop: @shop)
+    end
   end
 
   # GET /votes/1/edit
@@ -23,13 +30,16 @@ class VotesController < ApplicationController
 
   # POST /votes or /votes.json
   def create
-    @vote = Vote.new(vote_params)
+    @vote = current_user.votes.new(vote_params)
+
 
     respond_to do |format|
       if @vote.save
+        redirect_to @vote.shop, notice: '投票が成功しました。'
         format.html { redirect_to vote_url(@vote), notice: "Vote was successfully created." }
         format.json { render :show, status: :created, location: @vote }
       else
+        render :new
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @vote.errors, status: :unprocessable_entity }
       end

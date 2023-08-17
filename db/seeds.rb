653480028506f8b2c72f49a69a57e0debddfdaed
yuko <ryoyu-fu@live.jp>
2023-08-17 09:20:58 +0900

    before rails db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,116 +1,71 @@
-# db/seeds.rb
+# seeds.rb
 
-# カテゴリーの作成
+# Create categories
 categories = Category.create([
   { name: 'レストラン' },
-  { name: 'パン屋' },
-  { name: 'スイーツ' }
+  { name: 'ベーカリー' },
+  { name: 'スイーツ' },
+  { name: 'カフェ' },
   { name: 'その他' }
-  # 他のカテゴリーも追加
 ])
 
-# ショップの作成
-shops = [
-  {
-    name: 'ステーキハウス', 
-    prefecture: 1,  # プレフェクチャ (0-都道府県数-1)
-    address: '東京都渋谷区...', 
-    latitude: 35.12345,  # 緯度
-    longitude: 139.67890,  # 経度
-    category: categories[0]  # 関連するカテゴリー
-  },
-  {
-    name: 'カフェコーナー', 
-    prefecture: 2, 
-    address: '大阪府大阪市...', 
-    latitude: 34.56789, 
-    longitude: 135.12345, 
-    category: categories[1]
-  }
-  {
-    name: 'ルタオ本店', 
-    prefecture: 1, 
-    address: '北海道小樽市堺町７−１６', 
-    latitude: 43.191338, 
-    longitude: 141.007468, 
-    category: categories[0]
-  }
-  {
-    name: 'カフェコーナー', 
-    prefecture: 2, 
-    address: '大阪府大阪市...', 
-    latitude: 34.56789, 
-    longitude: 135.12345, 
-    category: categories[1]
-  }
-  {
-    name: 'カフェコーナー', 
-    prefecture: 2, 
-    address: '大阪府大阪市...', 
-    latitude: 34.56789, 
-    longitude: 135.12345, 
-    category: categories[1]
-  }
-  # 他のショップも追加
-]
-
-shops.each do |shop_data|
-  Shop.create(shop_data)
+# Create shops
+shops = []
+5.times do |i|
+  shop = Shop.new(
+    name: "Shop #{i + 1}",
+    address: "Address #{i + 1}",
+    category_id: categories.sample.id  # Corrected line
+  )
+  
+  # Set latitude and longitude
+  latitude = rand(35.0..37.0)
+  longitude = rand(135.0..137.0)
+  shop.latitude = latitude
+  shop.longitude = longitude
+  
+  # Set prefecture
+  prefecture = rand(0..46)
+  shop.prefecture = prefecture
+  
+  shop.save
+  shops << shop
 end
 
-# メニューの作成
-menus = [
-  { name: '特製ステーキセット', shop: shops[0] },
-  { name: 'カフェラテ', shop: shops[1] }
-  # 他のメニューも追加
-]
-
-menus.each do |menu_data|
-  Menu.create(menu_data)
+# Create menus
+menus = []
+shops.each do |shop|
+  5.times do |i|
+    menus << shop.menus.create(
+      name: "Menu #{i + 1} at #{shop.name}"
+    )
+  end
 end
 
-# ユーザーの作成
-users = [
-  {
-    email: 'john@example.com', 
-    password: 'password123', 
-    name: 'John Doe', 
-    admin: true,  # 管理者かどうか (true/false)
-    prefecture: 3  # プレフェクチャ (0-都道府県数-1)
-  },
-  {
-    email: 'jane@example.com', 
-    password: 'securepass456', 
-    name: 'Jane Smith', 
-    admin: false, 
-    prefecture: 4
-  }
-  # 他のユーザーも追加
-]
-
-users.each do |user_data|
-  User.create(user_data)
+# Create users
+users = []
+5.times do |i|
+  users << User.create(
+    name: "User #{i + 1}",
+    email: "user#{i + 1}@example.com",
+    password: 'password',
+    prefecture: rand(0..46)
+  )
 end
 
-# 投票の作成
-votes = [
-  {
-    shop: shops[0], 
-    user: users[0], 
-    menu: menus[0], 
-    content: '美味しいお肉！', 
-    image: 'https://example.com/image1.jpg'  # 画像の URL
-  },
-  {
-    shop: shops[1], 
-    user: users[1], 
-    menu: menus[1], 
-    content: '素敵なカフェです。', 
-    image: 'https://example.com/image2.jpg'  # 画像の URL
-  }
-  # 他の投票も追加
-]
-
-votes.each do |vote_data|
-  Vote.create(vote_data)
+# Create votes
+users.each do |user|
+  5.times do |i|
+    menu = menus.sample
+    Vote.create(
+      user: user,
+      shop: menu.shop,
+      menu: menu,
+      content: "Vote #{i + 1} by #{user.name}"
+    )
+  end
 end
+
+# Create guest users
+User.guest
+User.guest_admin

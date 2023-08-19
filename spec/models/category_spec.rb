8ed76e4eb5e7f2bc56d2124383f209b17608e37f
yuko <ryoyu-fu@live.jp>
2023-08-19 13:16:38 +0900

    before rspec

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "有効な属性であること" do
+    category = FactoryBot.build(:category)
+    expect(category).to be_valid
+  end
+
+  it "名前がない場合は無効であること" do
+    category = FactoryBot.build(:category, name: nil)
+    expect(category).to_not be_valid
+  end
 end
